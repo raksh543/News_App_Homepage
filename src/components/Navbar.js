107c6be5fe9d9f23440297e357f63ab067ecf714
DeepanjanBose <bose18deep@gmail.com>
2020-05-18 14:31:49 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,106 @@
+import React from "react";
+import * as ReactBootstrap from "react-bootstrap";
+import { Container } from "reactstrap";
+
+export default function Navbar() {
+  return (
+    <div>
+      <Container className="container-nav">
+        <ReactBootstrap.Navbar bg="dark" variant="dark" expand="lg">
+          <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
+            <ReactBootstrap.Nav className="mr-auto">
+              <ReactBootstrap.Nav.Link href="//">Home</ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">
+                Politics
+              </ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">World</ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">
+                Europe
+              </ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">
+                Economy
+              </ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">Money</ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">
+                Business
+              </ReactBootstrap.Nav.Link>
+
+              <ReactBootstrap.Nav.Link href="//">
+                Archive
+              </ReactBootstrap.Nav.Link>
+              <ReactBootstrap.NavDropdown
+                title="Features"
+                id="basic-nav-dropdown"
+              >
+                <ReactBootstrap.NavDropdown.Item href="/action/3.1">
+                  Action
+                </ReactBootstrap.NavDropdown.Item>
+                <ReactBootstrap.NavDropdown.Item href="/action/3.2">
+                  Another action
+                </ReactBootstrap.NavDropdown.Item>
+                <ReactBootstrap.NavDropdown.Item href="/action/3.3">
+                  Something
+                </ReactBootstrap.NavDropdown.Item>
+                <ReactBootstrap.NavDropdown.Divider />
+                <ReactBootstrap.NavDropdown.Item href="/action/3.4">
+                  Separated link
+                </ReactBootstrap.NavDropdown.Item>
+              </ReactBootstrap.NavDropdown>
+            </ReactBootstrap.Nav>
+          </ReactBootstrap.Navbar.Collapse>
+        </ReactBootstrap.Navbar>
+
+        {/* 2nd nav */}
+        <div className="navbar_two">
+          <ReactBootstrap.Navbar bg="light" expand="lg">
+            <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
+              <ReactBootstrap.Nav className="mr-auto">
+                <ReactBootstrap.Nav.Link href="//">
+                  Demo
+                </ReactBootstrap.Nav.Link>
+                <ReactBootstrap.Nav.Link href="//">
+                  More Demos
+                </ReactBootstrap.Nav.Link>
+                <ReactBootstrap.Nav.Link href="//">
+                  Government
+                </ReactBootstrap.Nav.Link>
+                <ReactBootstrap.Nav.Link href="//">
+                  Financial
+                </ReactBootstrap.Nav.Link>
+                <ReactBootstrap.Nav.Link href="//">
+                  Sports
+                </ReactBootstrap.Nav.Link>
+              </ReactBootstrap.Nav>
+            </ReactBootstrap.Navbar.Collapse>
+          </ReactBootstrap.Navbar>
+        </div>
+        {/* 3rd nav */}
+
+        <ReactBootstrap.Navbar bg="light" expand="lg" >
+          <ReactBootstrap.Nav className="mr-auto">
+          </ReactBootstrap.Nav>
+          <ReactBootstrap.Form inline>
+            <ReactBootstrap.FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+            <ReactBootstrap.Button variant="outline-dark">
+              Search
+            </ReactBootstrap.Button>
+          </ReactBootstrap.Form>
+        </ReactBootstrap.Navbar>
+
+        <br />
+      </Container>
+    </div>
+  );
+}
